eae4984f576c0d297c33bb45663bf4239c2aece0
jiyeonpark <jiyeonpark@mrblue.com>
2017-09-20 10:01:30 +0900

    - init

---
+++ JYToast/JYToast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JYToast
+//
+//  Created by 박지연 on 2017. 9. 20..
+//  Copyright © 2017년 박지연. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
